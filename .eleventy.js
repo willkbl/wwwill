2c0c3120a7eee139475346ff7cc64befb83a09cb
Will Kibel <56790114+willkbl@users.noreply.github.com>
2025-02-04 14:45:49 -0500

    tutorial progress

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 module.exports = config => {
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/');
+  //use addPassthroughCopy for any other directories you want
+  //hint, hint: music???
     return {
+      markdownTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
       dir: {
-        input: 'src/pages',
+        input: 'src',
         output: 'dist',
       },
     };
