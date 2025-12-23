e5f86393d617ea16d1e45b601cefba006bb2f289
Will Kibel <56790114+willkbl@users.noreply.github.com>
2025-12-23 16:14:33 -0500

    audio passthrough added
    checking deployment again

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/images");
+    eleventyConfig.addPassthroughCopy("./src/music");
 
     return {
         dir: {
